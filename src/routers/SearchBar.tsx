fbffc9f0c7d73d12eaeff8000f5f9343d851d8ea
Zelin <zelintian97@gmail.com>
2023-06-12 02:01:33 -0400

    update search bar position and add a search button

--- src/routers/SearchBar.tsx
+++ src/routers/SearchBar.tsx
@@ -1,13 +1,30 @@
-import { Tooltip, TextField } from '@mui/material';
-import * as React from 'react';
+import { Tooltip, TextField, Button } from "@mui/material";
+import * as React from "react";
 
 function SearchBar(props: any) {
+  const handleSearch = () => {
+    // Handle search functionality here
+  };
+
   return (
     <div className="search-bar-wrapper">
-        <Tooltip title="ie. Search all drop between 9am untill now" followCursor>
-            <TextField className="search-bar" id="filled-basic" label="Search Bar" variant="filled" />
-        </Tooltip>
-
+      <Tooltip title="ie. Search all drop between 9am untill now" followCursor>
+        <div className="search-bar-container">
+          <TextField
+            id="filled-basic"
+            label="Search Bar"
+            variant="filled"
+            className="search-bar"
+          />
+          <Button
+            variant="contained"
+            onClick={handleSearch}
+            className="search-button"
+          >
+            Search
+          </Button>
+        </div>
+      </Tooltip>
     </div>
   );
 }
