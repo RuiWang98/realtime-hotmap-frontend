12cfe1b302e5406ce5a5948621a6ac0cb8fbddee
TonyL <65515982+TYL1026@users.noreply.github.com>
2023-06-17 23:48:33 -0700

    search bar added

--- src/routers/SearchBar.tsx
+++ src/routers/SearchBar.tsx
@@ -1,15 +1,49 @@
-import { Tooltip, TextField } from '@mui/material';
+import { Tooltip, TextField, Button } from '@mui/material';
 import * as React from 'react';
 
-function SearchBar(props: any) {
-  return (
-    <div className="search-bar-wrapper">
-        <Tooltip title="ie. Search all drop between 9am untill now" followCursor>
-            <TextField className="search-bar" id="filled-basic" label="Search Bar" variant="filled" />
-        </Tooltip>
 
+function SearchBar(props: any) {
+  const [searchBarValue, setSearchBarValue] = React.useState('');
+  
+  const handleSearch = async () => {
+    fetch(`http://localhost:8080/api/get?query=${searchBarValue}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <div className="search-bar-wrapper">
+        <Tooltip title="ie. Search all drop between 9am until now" followCursor>
+          <TextField
+            className="search-bar"
+            id="filled-basic"
+            label="Search Bar"
+            variant="filled"
+            value={searchBarValue}
+            onChange={(event) => {
+              setSearchBarValue(event.target.value);
+            }}
+          />
+        </Tooltip>
+        <Button className="button-container" variant="contained" onClick={handleSearch}>
+          Search
+        </Button>
+      </div>
+
+      <div className="button-container">
+        <Button variant="contained" onClick={handleSearch}>
+          Search
+        </Button>
+      </div>
     </div>
   );
 }
 
 export default React.memo(SearchBar);
+
